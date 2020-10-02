8f814f2345d8ad3244279312ca6e8b53ca4bd7e3
Rob C <ric@oundleschool.org.uk>
2020-10-02 11:38:49 +0100

    Addtoclass done

--- addtoclass.php
+++ addtoclass.php
@@ -1,31 +1,16 @@
-<html>
-<title>Subjects</title>
-    
-</head>
-<body>
-   <form action="addtoclasses.php" method="post">
-   <select name = "student">
 <?php
-include_once('connection.php');
-$stmt = $conn->prepare("SELECT * FROM TblUser WHERE Role = 0 ORDER BY Surname ASC");
-$stmt->execute();
-
-
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-{
-echo('<option value='.$row["UserID"].'>'.$row["Surname"].', '.$row["Forename"].'</option>');
-}
-echo ("</select>");
-echo ("<select name = 'student'>");
-$stmt = $conn->prepare("SELECT * FROM TblSubjects  ORDER BY Subjectname ASC");
-$stmt->execute();
-
-
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-{
-echo('<option value='.$row["SubjectID"].'>'.$row["Subjectname"].'</option>');
-}
+header('Location: pupildoessubject.php');
+try{
+	include_once('connection.php');
+	array_map("htmlspecialchars", $_POST);
+	$stmt = $conn->prepare("INSERT INTO TblPupilStudies (SubjectID,UserID,Classposition, Classgrade, ExamMark,Comment)VALUES (:subjectid,:userid,NULL,NULL,NULL,NULL)");
+	$stmt->bindParam(':subjectid', $_POST["subject"]);
+	$stmt->bindParam(':userid', $_POST["student"]);
+	$stmt->execute();
+	}
+catch(PDOException $e)
+	{
+		echo "error".$e->getMessage();
+	}
+$conn=null;
 ?>
-</select>
-</body>
-</html>
---
+++ pupildoessubject.php
@@ -0,0 +1,32 @@
+<html>
+<title>Subjects</title>
+    
+</head>
+<body>
+   <form action="addtoclass.php" method="post">
+   <select name = "student">
+<?php
+include_once('connection.php');
+$stmt = $conn->prepare("SELECT * FROM TblUser WHERE Role = 0 ORDER BY Surname ASC");
+$stmt->execute();
+
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+{
+echo('<option value='.$row["UserID"].'>'.$row["Surname"].', '.$row["Forename"].'</option>');
+}
+echo ("</select>");
+echo ("<select name = 'subject'>");
+$stmt = $conn->prepare("SELECT * FROM TblSubjects  ORDER BY Subjectname ASC");
+$stmt->execute();
+
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+{
+echo('<option value='.$row["SubjectID"].'>'.$row["Subjectname"].'</option>');
+}
+?>
+</select>
+<input type="submit" value="Add pupil to subject">
+</body>
+</html>
