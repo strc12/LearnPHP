7e248a24886f7e6c8c8f93b92c2dc8d0d3eb7acf
Rob C <ric@oundleschool.org.uk>
2020-10-05 12:30:24 +0100

    basic login

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+   <title>Login</title>
+  
+</head>
+<body>
+
+<form action="loginprocess.php" method= "POST">
+ User name:<input type="text" name="Username"><br>
+ Password:<input type="password" name="Pword"><br>
+  <input type="submit" value="Login">
+</form>
+
+
+</body>
+</html>
