55e45d008d7b756a1fb2e060ec901f0bee636b98
Rob C <ric@oundleschool.org.uk>
2020-10-16 15:20:51 +0100

    Join method

--- showoptions.php
+++ showoptions.php
@@ -6,19 +6,16 @@
 <body>
 <?php
 include_once('connection.php');
-$stmt = $conn->prepare("SELECT * FROM Tblpupilstudies WHERE UserID=:selecteduser");
+$stmt = $conn->prepare("SELECT tblsubjects.Subjectname as sn FROM Tblpupilstudies 
+INNER JOIN tblsubjects 
+ON tblsubjects.SubjectID=tblpupilstudies.SubjectID 
+WHERE Tblpupilstudies.UserID=:selecteduser");
+
 $stmt->bindParam(':selecteduser', $_POST["Name"]);
 $stmt->execute();
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
 {
-    //method 1 - looking up using a separate query
-    $stmt2 = $conn->prepare("SELECT *  FROM tblsubjects WHERE SubjectID=:subject");
-    $stmt2->bindParam(':subject', $row["Subjectid"]);
-    $stmt2->execute();
-    while ($row2 = $stmt2->fetch(PDO::FETCH_ASSOC))
-    {
-        echo($row2["Subjectname"]."<br>");
-    }
+        echo($row["sn"]."<br>");
 }
 ?>	
 </form>
